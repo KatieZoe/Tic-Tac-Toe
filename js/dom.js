ec90b8d83e7094f89b7ccf4527029326dd692ea2
mac rent <macrent@macs-MacBook-Air-2.local>
2021-04-06 20:43:00 +1000

    version 2 - changing storage of player status using array instead of jquery

--- js/dom.js
+++ js/dom.js
@@ -1,45 +1,63 @@
 // document use: interacting with the DOM
+//clear  function all txt dissapear - call once winner is defined
 
-//put any functions here that you want to run before contacting the DOM (initial page requirements at the begining of the game)
-
-//empty function here
 
 $(document).ready(function () {
 
-const gameFunction = function (position, player) {
-
-////need function that allocates player to these and a separate function that checks the positions 
-  const pos0 = $('table #0');
-  const pos1 = $('table #1');
-  const pos2 = $('table #2');
-  const pos3 = $('table #3');
-  const pos4 = $('table #4');
-  const pos5 = $('table #5');
-  const pos6 = $('table #6');
-  const pos7 = $('table #7');
-  const pos8 = $('table #8');
-
-  const playerX = $('aside #X');
-  const playerO = $('aside #O');
-  //
+// for CSS - later to show which player is active.
+  // $('#O').on('click', function() {
+  //   $(this).addClass('selectO');
+  // });
   // $('#X').on('click', function() {
-  //  add a function that makes an X appear in the box clicked next (only allow 1 click)
-  // });
-  // $('#O').on('click',function (){
-  //  add a function that makes a O appear in the box clicked next (only allow 1 click)
+  //   $(this).addClass('selectX')
   // });
 
-  if(selection[0] != button("empty") && selection[0] === selection[1] && selection[1] === selection[2]){
-    print winning message with jquery.
+//change current player function
+  let currentPlayer = 'X';
+  const findCurrentPlayer = function() {
+    if(currentPlayer === 'X'){
+      return 'O';
+    }if(currentPlayer === 'O'){
+      return 'X';
+    }
+  };
+
+
+
+//loop - to change players each turn and adds in player reference to table
+const switchPlayer = function (){
+  for(let i = 0; i <= 8; i++){
+    $(`#${i}`).on('click', function(){
+      currentPlayer = findCurrentPlayer();
+      $(`#${i}`).text(currentPlayer)
+      returnWinningPlayer();
+      if(winner !== ''){
+        console.log(`The winner is ${ winner }`)
+      }
+      })
   }
+};
+switchPlayer();
+
+// const winMessage = function(){
+//   const playerX = 'X';
+//   const playerO = 'O';
+//   if(winner === playerX){
+//     console.log(playerX);
+//   }if(winner === playerO){
+//     console.log(playerO);
+//   }
+// }
+
+const pos0 = $('#0').text();
+const pos1 = $('#1').text();
+const pos2 = $('#2').text();
+const pos3 = $('#3').text();
+const pos4 = $('#4').text();
+const pos5 = $('#5').text();
+const pos6 = $('#6').text();
+const pos7 = $('#7').text();
+const pos8 = $('#8').text();
+// winMessage();
 
 }); // end document ready function
-
-
-// $('#checking-deposit').on('click', function () {
-//   const currentBalance = + $('#checking-balance').text().slice(1);
-//   const depositAmount = + $('#checking-amount').val();
-//   const newBalance =  currentBalance + depositAmount;
-//   $('#checking-balance').text('$' + newBalance);
-//   checkForZero();
-// });
